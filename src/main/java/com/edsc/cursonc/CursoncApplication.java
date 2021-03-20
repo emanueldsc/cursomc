bb19db31d7ad83ba98189b508d8869bd303adc77
emanueldsc <emanuel.douglas.sc@gmail.com>
2021-03-20 12:17:36 -0300

    Projeto Criado

---
+++ src/main/java/com/edsc/cursonc/CursoncApplication.java
@@ -0,0 +1,13 @@
+package com.edsc.cursonc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edsc/cursonc/CursoncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edsc.cursonc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
