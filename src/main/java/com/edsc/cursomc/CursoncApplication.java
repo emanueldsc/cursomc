e2cbb96734bb9990c15adc42a65cc981d4dd7ef2
emanueldsc <emanuel.douglas.sc@gmail.com>
2021-03-21 11:18:08 -0300

    Cria o banco de dados H2 automaticamente com Anotations

--- src/main/java/com/edsc/cursomc/CursoncApplication.java
+++ src/main/java/com/edsc/cursomc/CursoncApplication.java
@@ -1,4 +1,4 @@
-package com.edsc.cursonc;
+package com.edsc.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/edsc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/edsc/cursomc/resources/CategoriaResource.java
@@ -1,4 +1,4 @@
-package com.edsc.cursonc.resources;
+package com.edsc.cursomc.resources;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,23 +7,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.edsc.cursonc.domain.Categoria;
+import com.edsc.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
-
+	
+	private Integer id;
+	private String nome;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Categoria> listar() {
 		
-		Categoria cat1  = new Categoria(1, "Informática");
-		Categoria cat2  = new Categoria(2, "Escritótio");
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Informática");
 		
-		List<Categoria> lista = new ArrayList<>();
+		List<Categoria> lista = new ArrayList<Categoria>();
 		lista.add(cat1);
 		lista.add(cat2);
 		
-		return lista;	
+		return lista;
 	}
 
 }
