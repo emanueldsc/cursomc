78f97a1e5239b901191ddd248d5326cc96330ae8
emanueldsc <emanuel.douglas.sc@gmail.com>
2021-03-21 12:31:57 -0300

    Cria repository e service para categoria.

---
+++ src/main/java/com/edsc/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.edsc.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edsc.cursomc.domain.Categoria;
+import com.edsc.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
