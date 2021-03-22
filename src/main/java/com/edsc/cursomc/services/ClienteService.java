e30520e1ff09587c43a5f2fe0314d87480b387a2
emanueldsc <emanuel.douglas.sc@gmail.com>
2021-03-22 00:05:02 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/edsc/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.edsc.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edsc.cursomc.domain.Cliente;
+import com.edsc.cursomc.repositories.ClienteRepository;
+import com.edsc.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id:" + id + ", Tipo:" + Cliente.class.getName()));
+	}
+}
