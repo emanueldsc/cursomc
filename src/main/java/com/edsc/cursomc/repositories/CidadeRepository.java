b306f4a806a6ca99d6252af371114d2f2f6f78f1
emanueldsc <emanuel.douglas.sc@gmail.com>
2021-04-08 23:26:04 -0300

    Validação Customizda: email não repetido na inserção do cliente

--- src/main/java/com/edsc/cursomc/repositories/CidadeRepository.java
+++ src/main/java/com/edsc/cursomc/repositories/CidadeRepository.java
@@ -2,10 +2,13 @@ package com.edsc.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.edsc.cursomc.domain.Cidade;
+import com.edsc.cursomc.domain.Cliente;
 
 @Repository
 public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+	
 	
 }
